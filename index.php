2f0ca2a01e994a949cecacd4d42962cf7a47c56a
2011 Alexandre <2011brunofernandes@gmail.com>
2022-09-28 17:21:29 -0300

    carousel right - html

--- index.php
+++ index.php
@@ -1,15 +1,35 @@
 <?php get_header();?>
-<section class="ir-para-conteudo">
-    <div class="container">
-        <div class="row">
-            <div class="col-6">
-                <?php get_template_part('template-parts/home/carousel-left') ?> 
-            </div>
-            <div class="col-6">
-                <!-- <?php get_template_part('template-parts/home/carousel-right') ?>  -->
+<div class="ir-para-conteudo">
+    <section class="banner">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="carousel-banner">
+                        <div class="carousel-left">                        
+                            <?php get_template_part('template-parts/home/carousel-left') ?>
+                        </div>
+                        <div class="carousel-right">
+                            <?php get_template_part('template-parts/home/carousel-right') ?> 
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
+    <section class="category-highlight">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-9">
+                    col -9 
+                </div>
+                <div class="col-md-3">
+                    col 3
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+</div>
 
 <?php get_footer(); ?>
---
+++ template-parts/home/carousel-right.php
@@ -0,0 +1,18 @@
+<div class="carousel-content">
+    <div class="item">
+        <img src="" alt="">
+        <h2 class="subtitle">Lorem ipsum dolor sit amet.</h2>
+    </div>
+    <div class="item">
+        <img src="" alt="">
+        <h2 class="subtitle">Lorem ipsum dolor sit amet.</h2>
+    </div>
+    <div class="item">
+        <img src="" alt="">
+        <h2 class="subtitle">Lorem ipsum dolor sit amet.</h2>
+    </div>
+    <div class="item">
+        <img src="" alt="">
+        <h2 class="subtitle">Lorem ipsum dolor sit amet.</h2>
+    </div>
+</div>
