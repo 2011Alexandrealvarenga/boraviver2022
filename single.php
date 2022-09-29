79235e62c8ea4a030d5c7d937df491da1e07b590
2011 Alexandre <2011brunofernandes@gmail.com>
2022-09-29 10:53:04 -0300

    sobre o autor - html

--- single.php
+++ single.php
@@ -10,6 +10,30 @@
                         <?php the_title(); ?>
                     </h2>
                 </div>
+                <div class="creator mt-4">
+                    <span class="author"><b>Autor:</b> Alexandre Alvarenga</span>
+                    <span class="date"><b>Data:</b> 02/10/1985</span>
+                    <span class="category"><b>Categoria:</b> Biblia</span>
+                </div>
+                <div class="content-post mt-5">
+                    <span>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam deleniti adipisci, praesentium, officiis sint obcaecati beatae ea eos quas dignissimos sed dolorem dicta tenetur eligendi? Commodi dolores non vitae laudantium. Excepturi odio non provident, vero debitis rem autem iste illo laudantium quo eveniet voluptatem aliquid alias facere nostrum repellendus placeat eos! Numquam et quia vero, nobis corporis quaerat quis accusamus possimus maxime suscipit accusantium, modi minus. Aliquid sint deleniti quo amet unde accusamus voluptatibus, repellat quisquam ea numquam reiciendis officiis quod non officia repudiandae libero incidunt quaerat nesciunt, dicta eum cupiditate autem laboriosam tempore? Adipisci neque cum nostrum quisquam aut eum? Id, unde blanditiis. Itaque voluptates eaque voluptatibus laboriosam necessitatibus rerum in laudantium ipsa, distinctio aut veniam doloribus tenetur dolores doloremque repellendus velit nisi repudiandae? Et molestias vero dolores consectetur adipisci illum? Eos aliquid corrupti ullam fuga ut quaerat necessitatibus nihil molestias, eum, tempora, harum non ipsum esse! Voluptatum, neque.
+                    </span>
+                </div>
+                <div class="about-author mt-4">
+                    <h3 class="author">Sobre o autor</h3>
+                    <div class="align mt-2">
+                        <img src="http://localhost:8080/WP/boraviver/wp-content/uploads/2022/09/download-1.jpg" alt="">
+                        <div class="desc">    
+                            <h4 class="name"><b>Nome:</b> Alexandre Alvarenga</h4>
+                            <div class="description"> 
+                                <span>
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis, quo! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis, quo!
+                                </span> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="col-3">
                 sidebar
