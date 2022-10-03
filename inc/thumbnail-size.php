567f88eccebd9b1981ad7b041016685da2c73cd1
2011 Alexandre <2011brunofernandes@gmail.com>
2022-10-03 18:28:25 -0300

    carousel right

--- inc/thumbnail-size.php
+++ inc/thumbnail-size.php
@@ -3,13 +3,13 @@
 add_theme_support( 'post-thumbnails' );
 
 add_image_size( 'home-carousel-left', 555, 360, true ); //Imagem do carousel da home
+add_image_size( 'home-carousel-right', 278, 180, true ); //Imagem do carousel da home
 
 add_filter( 'intermediate_image_sizes_advanced', 'prefix_remove_default_images' );
 function prefix_remove_default_images( $sizes ) {
- unset( $sizes['small']); // 150px
- unset( $sizes['medium']); // 300px
- unset( $sizes['large']); // 1024px
- unset( $sizes['medium_large']); // 768px
- return $sizes;
+    unset( $sizes['small']); // 150px
+    unset( $sizes['medium']); // 300px
+    unset( $sizes['large']); // 1024px
+    unset( $sizes['medium_large']); // 768px
+    return $sizes;
 }
-
--- template-parts/home/carousel-right.php
+++ template-parts/home/carousel-right.php
@@ -1,18 +1,25 @@
+<?php 
+$my_args_banner = array(
+	'posts_per_page' => 4,
+	);
+	
+	// query
+	$my_query_banner = new WP_Query ( $my_args_banner );
+?>
 <div class="carousel-content">
+    <?php if( $my_query_banner->have_posts()) : 
+        while( $my_query_banner->have_posts() ) : 
+        $my_query_banner->the_post(); 
+    ?>
     <div class="item">
-        <img src="http://localhost:8080/WP/boraviver/wp-content/uploads/2022/09/download-1.jpg" alt="">
-        <h2 class="subtitle">Lorem ipsum dolor sit amet.</h2>
+        <?php if ( has_post_thumbnail() ) {
+            the_post_thumbnail('home-carousel-right', array('class' => '')) ;
+            } else { ?>
+            <img src="" alt="" title=""/>
+        <?php } ?>
+        <h2 class="subtitle"><?php the_title(); ?></h2>
+
     </div>
-    <div class="item">
-        <img src="http://localhost:8080/WP/boraviver/wp-content/uploads/2022/09/download-1.jpg" alt="">
-        <h2 class="subtitle">Lorem ipsum dolor sit amet.</h2>
-    </div>
-    <div class="item">
-        <img src="http://localhost:8080/WP/boraviver/wp-content/uploads/2022/09/download-1.jpg" alt="">
-        <h2 class="subtitle">Lorem ipsum dolor sit amet.</h2>
-    </div>
-    <div class="item">
-        <img src="http://localhost:8080/WP/boraviver/wp-content/uploads/2022/09/download-1.jpg" alt="">
-        <h2 class="subtitle">Lorem ipsum dolor sit amet.</h2>
-    </div>
+    <?php endwhile; else : ?>
+    <?php endif; ?>
 </div>
