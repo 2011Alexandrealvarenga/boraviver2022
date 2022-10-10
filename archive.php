0838bb6e967bcc04fd6769e8e6be1549bb867aaa
2011 Alexandre <2011brunofernandes@gmail.com>
2022-10-10 11:20:18 -0300

    archive pagina

---
+++ archive.php
@@ -0,0 +1,24 @@
+<?php get_header();?>
+<section class="resume-post">
+    <div class="container">
+        <div class="row">
+            <div class="col-9">
+                <hr class="line">
+                <h2 class="subtitle"><?php the_archive_title(); ?></h2>
+                <?php get_template_part('template-parts/more-posts') ;?> 
+            </div>
+            <div class="col-3">
+                <?php
+                if (have_posts()) :
+                    while (have_posts()) : the_post();
+                    ?>
+                    <a href="<?php the_permalink(); ?>">
+                        <?php the_title(); ?>
+                    </a>
+                <?php endwhile; else : ?>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+</section>
+<?php get_footer(); ?>
