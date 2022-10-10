e2eb0ba41f02e612c628d30aecfb83affd6da8d5
2011 Alexandre <2011brunofernandes@gmail.com>
2022-10-10 15:44:16 -0300

    page

---
+++ page.php
@@ -0,0 +1,15 @@
+<?php get_header();?>
+<section class="page">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <?php while(have_posts()) : the_post();?>
+                    <h1><?php the_title(); ?></h1>
+                    
+                    <?php the_content(); ?>
+                <?php endwhile; ?>
+            </div>
+        </div>
+    </div>
+</section>
+<?php get_footer(); ?>
