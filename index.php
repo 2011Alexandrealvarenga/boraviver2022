b2728024c67e7e7bd32564913e2a0ce88c3b1d95
2011 Alexandre <2011brunofernandes@gmail.com>
2022-10-13 14:06:13 -0300

    banner image

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php get_header();?>
 <div class="ir-para-conteudo">
-    <section class="banner">
+    <section class="banner-imagem">
+        <div class="bg-content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 ">
+                        <div class="content">
+                            <h1 class="title">Deus, Viagem, Biblia ... viver!</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="banner-carousel">
         <div class="container">
             <div class="row">
                 <div class="col-12">
