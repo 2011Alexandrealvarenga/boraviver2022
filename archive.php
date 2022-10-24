6736401d34717d5367e3a475c35505b9b9cd0347
2011 Alexandre <2011brunofernandes@gmail.com>
2022-10-24 15:04:19 -0300

    more posts

--- archive.php
+++ archive.php
@@ -1,19 +1,29 @@
 <?php get_header();?>
-<section class="resume-post">
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-12 col-md-9">
-                <hr class="line">
-                <h2 class="subtitle"><?php the_archive_title(); ?></h2>
-                <div class="row">
-
-                    <?php get_template_part('template-parts/more-posts') ;?> 
+    <section class="resume-post">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-md-9">
+                    <hr class="line">
+                    <h2 class="subtitle">Mais Notícias</h2>                    
+                  
+                    <?php if( have_posts()) :     
+                        while( have_posts() ) : 
+                        the_post(); 
+                    ?>
+                    <?php get_template_part('template-parts/more-posts') ;?>                        
+                    <?php endwhile; ?> 
+                    <?php get_template_part('template-parts/pagination') ;?> 
+                
+                    <?php else : ?>
+                        <p>Não ha dados para ser exibido</p>
+                    <?php endif; ?>
+                   
+                </div>
+                <div class="col-3 sidebar">
+                    <?php get_sidebar(); ?>
                 </div>
             </div>
-            <div class="col-3 sidebar">
-                <?php get_sidebar(); ?>
-            </div>
         </div>
-    </div>
-</section>
+    </section>
+
 <?php get_footer(); ?>
