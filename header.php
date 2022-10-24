86746aa325c74b69e037380c7512c53bd3c0e5df
2011 Alexandre <2011brunofernandes@gmail.com>
2022-10-24 13:50:00 -0300

    favicon

--- header.php
+++ header.php
@@ -11,6 +11,7 @@
     <meta charset="<?php bloginfo('charset');?> ">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri();?>/assets/img/favicon.png" />
     <?php wp_head(); ?>
 </head>
 <body <?php body_class();?>>
--- template-parts/thumb-default.php
+++ template-parts/thumb-default.php
@@ -1,1 +1,1 @@
-<img src="https://www.boraviver.space/wp-content/uploads/2022/10/pexels-steve-johnson-963056-1-scaled.jpg" alt="" title=""/>
+<img src="https://www.boraviver.space/wp-content/uploads/2022/10/pexels-steve-johnson-963056-1-scaled.jpg" class="without-thumb" alt="" title=""/>
