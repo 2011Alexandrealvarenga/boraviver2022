9ee2bab0af4f2dd14f3c62addcc4b1dd876e7929
2011 Alexandre <2011brunofernandes@gmail.com>
2022-10-25 19:01:57 -0300

    link alter width

--- search.php
+++ search.php
@@ -5,29 +5,22 @@
             <div class="col-sm-12 col-md-9">
                 <hr class="line">
                 <h2 class="subtitle mb-2">Resultado da busca: <?php echo get_search_query(); ?></h2> 
+                
                 <?php get_search_form(); ?>
-                
-                    <?php if (have_posts()) :
-                    while (have_posts()) : 
-                    the_post();
-                    ?>
-                        <?php if('post' == get_post_type()) :?>
-                            <?php get_template_part('template-parts/more-posts') ;?>                        
-                        <?php endif; ?>
-                    <?php endwhile; ?> 
-                        <?php get_template_part('template-parts/pagination') ;?> 
                     
-                    <?php else : ?>
-                        <p>Não ha dados para ser exibido</p>
+                <?php if (have_posts()) :
+                while (have_posts()) : 
+                the_post();
+                ?>
+                    <?php if('post' == get_post_type()) :?>
+                        <?php get_template_part('template-parts/more-posts') ;?>                        
                     <?php endif; ?>
-
+                <?php endwhile; ?> 
+                    <?php get_template_part('template-parts/pagination') ;?> 
                 
-
-                <!-- inicio  -->
-
-                <!-- fim -->
-
-
+                <?php else : ?>
+                    <p>Não ha dados para ser exibido</p>
+                <?php endif; ?>
             </div>
             <div class="col-3 sidebar">
                 <?php get_sidebar(); ?>
@@ -35,5 +28,6 @@
         </div>
     </div>
 </section>
+
 
 <?php get_footer(); ?>
