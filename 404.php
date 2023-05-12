871b77764047f8d42c222dd6c3aa6f903e76ecde
2011Alexandrealvarenga <2011brunofernandes@gmail.com>
2023-05-12 15:52:02 -0300

    busca alteracao

--- 404.php
+++ 404.php
@@ -1,10 +1,38 @@
 <?php get_header();?>
-<div class="container">
+<style>
+  .sec-404 {
+    height: 60vh;
+    display: flex;
+    align-items: center;
+  }
+  .sec-404 .content {
+    text-align: center;
+    display: grid;
+    justify-content: center;
+    }
+    .sec-404 .content form {
+        display: flex;
+        justify-content: center;
+    }
+</style>
+<section class="sec-404">
+  <div class="container">
     <div class="row">
-        <div class="col-12">
-            page 404
+      <div class="col-12">
+        <div class="content">
+          <h2 class="title">Resultado não encontrado</h2>
+          <h3 class="title">Erro - 404</h3>
+          <a href="/" alt="ir para 404 pagina inicial" title="404 pagina inicial"><p>Retorne a página inicial</p></a>
+          <form class="form-inline my-2 my-lg-0">
+            <?php get_search_form(); ?>
+            <!-- <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"> -->
+            <!-- <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> -->
+            </form>
         </div>
+      </div>
     </div>
-</div>
+  </div>
+</section>
+
 
 <?php get_footer(); ?>
--- searchform.php
+++ searchform.php
@@ -2,9 +2,41 @@
                 
     <div>
         <label class="screen-reader-text" for="s">Pesquisar por:</label>
-        <input type="text" value="" name="s" id="s">
-        <input type="submit" id="searchsubmit" value="Pesquisar">
+        <div class="content-form">
+            <input type="text" value="" name="s" id="s">            
+            <button type="submit" id="searchsubmit">
+                <img src="http://www.boraviver.space/wp-content/themes/boraviver2022/assets/img/search.svg" >
+            </button>
+        </div>
+
         <input type="hidden" value="post" name="post_type" id="post_type">
 
     </div>
 </form>
+<style>
+    .navbar #searchsubmit img,
+    form #searchsubmit img {
+        width: 17px;
+        /* padding: 8px; */
+    }
+    .navbar button#searchsubmit,
+    button#searchsubmit {
+        /* background: lightblue; */
+        /* width: 30px; */
+        /* height: 30px; */
+        /* padding: 2px; */
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        border: none;
+        background: none;
+    }
+    .navbar .content-form,
+    #searchform .content-form {
+        /* height: 100px; */
+        /* background: yellow; */
+        /* justify-content: center; */
+        display: flex;
+        align-items: center;
+    }
+</style>
